f9feca648d62e20a6a0fdc9b4eac26aff7d2e857
Ivon Liu <ivon.liu2011@gmail.com>
2016-01-13 22:38:22 -0800

    Delete useless

--- test.js
+++
@@ -1,9 +0,0 @@
-var chess = require('./garbochess.js');
-
-chess.resetGame();
-chess.initializeFromFen('rnbqkbnr/pp3ppp/2p1p3/1B1P4/4p3/5P2/PPP3PP/RNBQK1NR b KQkq -');
-console.log(chess.getFen());
-
-chess.search(function(bestMove, value, timeTaken, ply){
-  console.log(chess.getMoveSAN(bestMove));
-}, 99, null);
